39aa77576efd43aad8206d8124ced853a998103c
moba0222 <mohammad.babu.miya@netcracker.com>
2024-10-24 15:11:32 +0530

    practice1

--- Week1/Arrays/Problems/TwoSum/TwoSum-practice.js
+++ Week1/Arrays/Problems/TwoSum/TwoSum-practice.js
@@ -1,42 +1,32 @@
-console.log("Brute Force apprach O(n^2)O(nlogn), O(1)");
-// brute force approach
-function twoSum(nums,target){
+var twoSum = function(nums, target) {
     let n = nums.length;
-    for(let i = 0; i < n; i++){
-        for(let j = i + 1; j < n; j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j];
+    // ----------------- brute force approach -------------------------------
+    // for(i = 0; i < n; i++){
+    //     for(j = i+1; j < n; j++){
+    //         if(nums[i] + nums[j] === target){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
+    // ----------------- two pointer approach -------------------------------
+    let original = [...nums];
+    nums.sort((a,b)=> a-b)
+    let l = 0, r = n-1;
+    while(l < r){
+        let sum = nums[l] + nums[r];
+        if(sum === target){
+            if(nums[l] === nums[r]){
+                return [original.indexOf(nums[l]), original.lastIndexOf(nums[l])];
+            }else {
+                return [original.indexOf(nums[l]), original.indexOf(nums[r])];
             }
         }
-    }
-}
-//console.log(twoSum([1,2,3,4], 7));
-
-console.log("Two Pointer Apprach O(nlogn), O(1)");
-
-function twoSum1(nums, target){
-    // two pointers approach
-    // cal length
-    let n = nums.length;
-    // sort the array in ASC
-    nums.sort((a,b)=> a-b);
-    // declare & initialise l, r pointers
-    let l = 0, r = n-1;
-    while(l , r){
-        // iterate throug l<r and calcuate sum
-        let sum = nums[l] + nums[r];
-        // check for the expected condition met
-        // if yes return [l,r]
-        if(sum === target){
-            return [l,r];
-        }
-        // if no check for sum > t --> r--, sum < t --> l++
         if(sum > target){
             r--;
         }else{
-            l++;
+            l++
         }
-    } 
-}
+    }
 
-console.log(twoSum1([1,2,3,4], 7));
+    // ----------------- has map approach -------------------------------
+};
